798e1b69a39b8742b5b37d55cc4188482f6db949
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-02-11 12:43:55 -0500

    Remove last project comment

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,6 @@
-/*
-10.) 3 - 4 hrs (depending on the # of questions): Simple Quiz Game: Create a quiz game where the user is asked questions, and they have to select the correct answer from multiple choices. You can store the questions in a data file.
-*/
-
 import examples.* 
-@main
-def main() =
+
+@main def main() =
     Examples.taskManagerExample
     Examples.calculatorExample
     Examples.todoExample
